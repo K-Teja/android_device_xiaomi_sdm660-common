748777641460ff8786304041953e5f0decc8e15c
K-Teja <tejachaitu96@gmail.com>
2020-03-06 10:22:23 +0530

    twolip: copy codec to media_codecs_vendor instead of media_codecs
    Required after AOSPA/android_frameworks_av@496c253
    
    by Akhil Narang
    
    Signed-off-by: K-Teja <tejachaitu96@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -307,7 +307,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
